24299e953fd6120876ab1ab29ac101b954b87f21
chrispatterson <dimmerswitch@gmail.com>
2009-12-10 15:34:39 -0600

    Major update - SuperSelectors will now automatically create CSS for you

--- jquery.super-selectors.js
+++ jquery.super-selectors.js
@@ -3,7 +3,7 @@
  * A jQuery plugin enabling better CSS selector support for older browsers
  *  by leveraging jQuery's excellent selectors
  * 
- * Version 1.0
+ * Version 1.1
  * Author: Chris Patterson
  *
  * License: GPL 3 http://www.gnu.org/licenses/gpl-3.0.html
@@ -11,6 +11,8 @@
  **/
 (function($){
   $.fn.superSelectify = function(options) {
+  
+  var dynamicCSS = "";
 
   var defaults = {
    emptyClass: "empty",
@@ -47,15 +49,24 @@
   }
   
   function getMatches(CSS) {
-  
+    // We need to strip out any comments to make sure we don't apply styles inappropriately
+    CSS = CSS.replace(/\/\*[\s\S]*?\*\//g, '');
+    dynamicCSS = CSS;
+
     function _match_item(reg, className) {
-      var itemMatch = CSS.replace(/[\n\r]/gi, '').match(reg);
+	    var fullSelector = new RegExp('[a-zA-Z0-9._+~#:\\s-]*' + reg, "gi");
+			var classSelector = new RegExp(reg, "gi");
+      var itemMatch = CSS.replace(/[\n\r]/gi, '').match(fullSelector);
       if(itemMatch) itemMatch=itemMatch.join(", ");
       if(itemMatch) $(itemMatch).addClass(className);
+
+			dynamicCSS = dynamicCSS.replace(classSelector, '.' + className);
     }
 
     function _match_hover(reg, className) {
-      var itemMatch = CSS.replace(/[\n\r]/gi, '').match(reg);
+	    var fullSelector = new RegExp('[a-zA-Z0-9._+~#:\\s-]*' + reg, "gi");
+			var classSelector = new RegExp(reg, "gi");
+      var itemMatch = CSS.replace(/[\n\r]/gi, '').match(fullSelector);
       if(itemMatch) itemMatch=itemMatch.join(", ");
       if(itemMatch) $(itemMatch).hover(
        function() {
@@ -65,35 +76,34 @@
         $(this).removeClass(className);
        }
       );
+			dynamicCSS = dynamicCSS.replace(classSelector, '.' + className);
     }
     
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*:empty/gi, options.emptyClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*:first[^-]/gi, options.firstClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*:last[^-]/gi, options.lastClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*:nth-child\(odd\)/gi, options.oddClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*:nth-child\(even\)/gi, options.evenClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*?\+\s?[a-zA-Z0-9._+~#:-]*/gi, options.nextClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*?\~\s?[a-zA-Z0-9._+~#:-]*/gi, options.siblingClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*:first-child/gi, options.firstChildClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*:last-child/gi, options.lastChildClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*:only-child/gi, options.onlyChildClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*\>\s?[a-zA-Z0-9._+~#:-]*/gi, options.directChildClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*input\[type="text"\]/gi, options.textInputClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*input\[type="password"\]/gi, options.passwordInputClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*input\[type="radio"\]/gi, options.radioInputClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*input\[type="checkbox"\]/gi, options.checkboxInputClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*input\[type="submit"\]/gi, options.submitInputClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*input\[type="image"\]/gi, options.imageInputClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*input\[type="reset"\]/gi, options.resetInputClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*input\[type="button"\]/gi, options.buttonInputClass);
-    _match_item(/[a-zA-Z0-9._+~#:\s-]*input\[type="file"\]/gi, options.fileInputClass);
+		_match_item(':empty', options.emptyClass);
+		_match_item('(:first[^-])', options.firstClass);
+		_match_item('(:last[^-])', options.lastClass);
+		_match_item('(:nth-child\\(odd\\))', options.oddClass);
+		_match_item('(:nth-child\\(even\\))', options.evenClass);
+		_match_item('([a-zA-Z0-9._+~#:-]+\\s?\\+\\s?[a-zA-Z0-9._+~#:-]+)', options.nextClass);
+		_match_item('([a-zA-Z0-9._+~#:-]+\\s?\\~\\s?[a-zA-Z0-9._+~#:-]+)', options.siblingClass);
+		_match_item('(:first-child)', options.firstChildClass);
+		_match_item('(:last-child)', options.lastChildClass);
+		_match_item('(:only-child)', options.onlyChildClass);
+		_match_item('(\\>\\s?[a-zA-Z0-9._+~#:-]*)', options.directChildClass);
+		_match_item('(input\\[type="text"\\])', options.textInputClass);
+		_match_item('(input\\[type="password"\\])', options.passwordInputClass);
+		_match_item('(input\\[type="radio"\\])', options.radioInputClass);
+		_match_item('(input\\[type="checkbox"\\])', options.checkboxInputClass);
+		_match_item('(input\\[type="submit"\\])', options.submitInputClass);
+		_match_item('(input\\[type="image"\\])', options.imageInputClass);
+		_match_item('(input\\[type="reset"\\])', options.resetInputClass);
+		_match_item('(input\\[type="button"\\])', options.buttonInputClass);
+		_match_item('(input\\[type="file"\\])', options.fileInputClass);
 
 		// Also add hover listeners as needed
-    _match_hover(/[a-zA-Z0-9._+~#:\s-]*:hover/gi, options.hoverClass);
+    _match_hover(':hover', options.hoverClass);
 
     // Check for any imports within the passes CSS
-    // Only IE should ever hit this (other browsers 
-    //  will return them within ruleIterator)
     var importedCSS = CSS.match(/[a-zA-Z0-9\.\-_\+\s]*import([a-zA-Z0-9\.\-_\+\/]*\.css)/gi);
 
     if (importedCSS) {
@@ -104,16 +114,9 @@
       }  
     }
 
-  }
+		// Add our dynamic CSS to the <head>
+    $('<style type="text/css" media="all">' + dynamicCSS + '</style>').appendTo('head');
 
-  // Needed for non-IE browsers, as they don't support the cssText method for full stylesheets
-  function ruleIterator(sheet) {
-    var css = sheet.cssRules;
-    for(var rule=0;rule<css.length;rule++) {
-      if(css[rule].styleSheet) getCSS(css[rule].styleSheet);
-      if(css[rule].selectorText == null) continue;
-      getMatches(css[rule].selectorText);
-    }
   }
 
   // Retrieve the CSS if it's a link or import, otherwise process the embedded CSS
@@ -124,14 +127,9 @@
       var href = (RELATIVE.test(sheet.href)) ? (docURL.slice(0, docURL.lastIndexOf("/") + 1) + sheet.href) : sheet.href;
       $.ajax({ url: href, success: function(response){ getMatches(response); } });
     } else {
-      if(sheet.cssText) {
-        getMatches(sheet.cssText);
-      } else {
-        ruleIterator(sheet);
-      }
+      getMatches(sheet.innerHTML);
     } 
   }
-
 
   // If manual selectors have been provided, apply those first
   if(options.manualSelectors) getMatches(options.manualSelectors);
@@ -146,9 +144,9 @@
        return;
      }
 
-    for(stylesheet=0;stylesheet<document.styleSheets.length;stylesheet++) {
-      getCSS(document.styleSheets[stylesheet]);
-    };
+     $("style,link[type=text/css]").each(function () {
+	    getCSS(this);
+     });
   }
 
 };
